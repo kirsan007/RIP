2944ff1d347c99d79f0955a43d9b4653ff23d2cd
Кирсан <kirsan@MacBook-Pro-Kirsan.local>
2020-10-22 16:53:55 +0300

    add lab 1,2,3

---
+++ Lab2/main.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+from lab_python_oop.Rectangle import Rectangle
+from lab_python_oop.Circle import Circle
+from lab_python_oop.Square import Square
+import arrow
+
+
+def main():
+    print("ИУ5-51Б Пахомкин Кирсан Лаб №2")
+    print(arrow.now(), "\n")
+
+    rectangle = Rectangle("синего", 2, 2)
+    circle = Circle("зеленого", 2)
+    square = Square("красного", 2)
+
+    print(rectangle)
+    print(circle)
+    print(square)
+
+
+if __name__ == "__main__":
+    main()
